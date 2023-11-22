e00b9135dfd651acbdf101b8ed5e33718fc4851e
Juliano Cesar Magoga <julianoc.m@hotmail.com>
2023-11-22 14:30:03 -0300

    adding style to wrong answers

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,11 @@
 function temperatureConverter() {
-  const valueFahrenheit = document.querySelector("#valueFahrenheit")
+  const valueConvertionOne = document.querySelector("#valueConvertionOne").value
+  const valueConvertionTwo = document.querySelector("#valueConvertionTwo").value
+  let isValid = true
+  if (valueConvertionOne == valueConvertionTwo) {
+    isValid = false
+    adicionarNaTela(isValid)
+  }
   const calculateFarenheitToCelcius = ((valueFahrenheit.value - 32) * 5) / 9
   const calculateFarenheitToKelvin =
     ((valueFahrenheit.value - 32) * 5) / 9 + 273.15
@@ -11,7 +17,14 @@ function temperatureConverter() {
     valueFahrenheit.value
   }°F, convertido para Kelvin fica ${calculateFarenheitToKelvin.toFixed(2)}°K`
 
-  const resp = document.querySelector("#resp")
   resp.classList.add("resp")
   return (resp.innerHTML = msg)
+}
+
+function adicionarNaTela(isValid) {
+  const resp = document.querySelector("#resp")
+  if (!isValid) {
+    resp.classList.add("erro")
+    resp.innerText = "Conversão errada! Valores iguais!"
+  }
 }
