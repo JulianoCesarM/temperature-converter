eaf8e48ee7b9128bd2989cc02aaaae41dadd4684
Juliano Cesar Magoga <julianoc.m@hotmail.com>
2023-11-22 15:34:47 -0300

    created the addOnScreen function

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,30 +1,17 @@
 function temperatureConverter() {
-  const valueConvertionOne = document.querySelector("#valueConvertionOne").value
-  const valueConvertionTwo = document.querySelector("#valueConvertionTwo").value
   let isValid = true
-  if (valueConvertionOne == valueConvertionTwo) {
-    isValid = false
-    adicionarNaTela(isValid)
-  }
-  const calculateFarenheitToCelcius = ((valueFahrenheit.value - 32) * 5) / 9
-  const calculateFarenheitToKelvin =
-    ((valueFahrenheit.value - 32) * 5) / 9 + 273.15
-  msg = `Farenheit ${
-    valueFahrenheit.value
-  }°F, convertido para Celcius fica ${calculateFarenheitToCelcius.toFixed(
-    2
-  )}°C</br>Farenheit ${
-    valueFahrenheit.value
-  }°F, convertido para Kelvin fica ${calculateFarenheitToKelvin.toFixed(2)}°K`
 
-  resp.classList.add("resp")
-  return (resp.innerHTML = msg)
+  msg = `OI`
+  return addOnScreen(isValid, msg)()
 }
 
-function adicionarNaTela(isValid) {
-  const resp = document.querySelector("#resp")
+// Adiciona Todas as resposta para o usuario e erros
+function addOnScreen(isValid, msgResp) {
+  const inputResp = document.querySelector("#inputResp")
   if (!isValid) {
-    resp.classList.add("erro")
-    resp.innerText = "Conversão errada! Valores iguais!"
+    inputResp.classList.add("erro")
+    return (inputResp.innerText = msgResp)
   }
+  inputResp.classList.add("resp")
+  return (inputResp.innerText = msgResp)
 }
